a794edcb25d1de2bf33d30d29ea46e25aa002c08
Nicholas Bryant <nicholas@Nicholass-MacBook-Pro.local>
2017-03-13 12:15:49 -0400

    drafted base file structure and configured package.json

---
+++ open-scores-app/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAQAQswcL37GyLYtJWoUWwWuOXE9utlETQ",
+    authDomain: "react-bistro-nicholas.firebaseapp.com",
+    databaseURL: "https://react-bistro-nicholas.firebaseio.com"
+});
+
+export default base;
---
+++ open-scores-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {browserHistory, Router, Route, IndexRoute} from 'react-router';
+
+import App from './components/App';
+import './css/App.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'));
