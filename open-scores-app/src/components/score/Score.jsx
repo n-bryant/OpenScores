9e68d6ed1f0c3eadd826854b754b87bba4900fd0
Nicholas Bryant <nicholas@Nicholass-MacBook-Pro.local>
2017-03-14 09:06:26 -0400

    added ability to enter and edit title

--- open-scores-app/src/components/score/Score.jsx
+++ open-scores-app/src/components/score/Score.jsx
@@ -2,13 +2,41 @@ import React, {Component} from 'react';
 import VFDisplay from './Vex';
 
 class Score extends Component {
+  // on title hover, display edit icon
+  // on title click, toggle edit-title-form
+  // on edit-title-form submit, update title prop and display new value
+  processForm(event) {
+    event.preventDefault();
+    const newTitle = this.title.value;
+
+    this.props.editTitle(newTitle);
+    this.form.reset();
+    this.form.classList.add('is-hidden');
+    this.titleEl.classList.remove('is-hidden');
+  }
+
+  toggleTitleForm() {
+    this.titleEl.classList.add('is-hidden');
+    this.form.classList.remove('is-hidden');
+  }
 
   render() {
-
     return (
       <section className="score-wrapper">
         <div className="score-container">
-          <h2 className="score-title is-centered">Score Title</h2>
+          <div className="title-container">
+            <img className="edit-title-icon is-hidden" src="edit-icon" alt="edit-icon"/>
+            {/* TITLE ISN'T RECOGNIZING PROP VALUE */}
+            <h2 ref={(scoreTitle) => this.titleEl = scoreTitle} className="score-title is-centered" onClick={this.toggleTitleForm.bind(this)}>{this.props.title}</h2>
+            <form ref={(fullForm) => this.form = fullForm} className="edit-title-form is-hidden" name="edit-title-form" onSubmit={this.processForm.bind(this)}>
+              <legend className="is-hidden">Edit Title</legend>
+              <label htmlFor="title" className="is-hidden"/>
+              <input className="title-input is-centered" ref={(inputTitle) => this.title = inputTitle} type="text" id="title" placeholder={this.props.title} required autoFocus/>
+              <label className="is-hidden" type="submit" value="Update Title">
+                <input type="submit" value="Make Reservation"/>
+              </label>
+            </form>
+          </div>
           <VFDisplay />
         </div>
       </section>
