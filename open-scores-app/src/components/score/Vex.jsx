a2ca26a64dd408271a5c3d814f72e1c279df31f5
Nicholas Bryant <nicholas@Nicholass-MacBook-Pro.local>
2017-03-14 15:25:10 -0400

    extracted drawing of canvas to its own function, defined method to clear canvas

--- open-scores-app/src/components/score/Vex.jsx
+++ open-scores-app/src/components/score/Vex.jsx
@@ -9,8 +9,8 @@ class VFDisplay extends Component {
     const VF = Vex.Flow;
 
     // Create an SVG renderer and attach it to the DIV element named "VFDisplay".
-    const div = document.getElementById('vfDisplay');
-    let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    const vfDisplay = document.getElementById('vfDisplay');
+    let renderer = new VF.Renderer(vfDisplay, VF.Renderer.Backends.SVG);
 
     // Configure the rendering context.
     renderer.resize(1220, 500);
@@ -28,6 +28,7 @@ class VFDisplay extends Component {
 
     // create 1 note, push it to notes array, and draw that 1 note to page.
       notes[0] = new VF.StaveNote({clef:"treble", keys: ["c/3"], duration: "q"});
+    drawScore();
     // work on selecting that one note by clicking, keypress
     // controls do various things that update that note, change its value in the notes array to a new StaveNote object with different values;
       // redraw with new notes array values
@@ -35,23 +36,29 @@ class VFDisplay extends Component {
     // iterate over all bars, iterate over note positions, create invisible rest to be able to select
     // when a 'selecotor' is clicked, user able to put a note
     // that note updates the value at the index in the stave/note/array
-    for (let i = 0; i < barCount; i++) {
-      if (i === 0) {
-        // Create a stave of width 300 at position 10, 40 on the canvas.
-        staveBars[i] = new VF.Stave(staveX, staveY, staveWidth);
+    function drawScore() {
+      context.clear();
+      for (let i = 0; i < barCount; i++) {
+        if (i === 0) {
+          // Create a stave of width 300 at position 10, 40 on the canvas.
+          staveBars[i] = new VF.Stave(staveX, staveY, staveWidth);
 
-        // Add a clef and time signature.
-        staveBars[i].addClef("treble").addTimeSignature("4/4");
-      } else {
-        staveBars[i] = new VF.Stave(staveBars[i - 1].width + staveBars[i - 1].x, staveY, staveWidth);
+          // Add a clef and time signature.
+          staveBars[i].addClef("treble").addTimeSignature("4/4");
+        } else {
+          staveBars[i] = new VF.Stave(staveBars[i - 1].width + staveBars[i - 1].x, staveY, staveWidth);
+        }
+
+        // notes[i] = [];
+        // Connect it to the rendering context and draw
+        staveBars[i].setContext(context).draw();
+
+        VF.Formatter.FormatAndDraw(context, staveBars[i], notes);
       }
-
-      // notes[i] = [];
-      // Connect it to the rendering context and draw
-      staveBars[i].setContext(context).draw();
-
-      VF.Formatter.FormatAndDraw(context, staveBars[i], notes);
     }
+
+    // notes[0] = new VF.StaveNote({clef:"treble", keys: ["g/4"], duration: "q"});
+    // drawScore();
 
 
 
