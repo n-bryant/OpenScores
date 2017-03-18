03ea6d60b55023fb214696aab828b793af1f8f97
carter magee <cartermagee01@gmail.com>
2017-03-18 16:33:59 -0400

    created Playback.jsx Toolbox.jsx - moved the playback buttons and toggleToolBoxPosition() into Playback.jsx

--- open-scores-app/src/components/score/ToolBox.jsx
+++ open-scores-app/src/components/score/ToolBox.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Playback from './Playback';
 
 class ToolBox extends Component {
 
@@ -19,36 +20,7 @@ class ToolBox extends Component {
     return (
       <div>
         <div className="tool-box horizontal is-flex">
-          <div className="playback-controls is-flex">
-            <div className="playback-buttons">
-              <button className="playback stopBtn is-centered">
-                <img src="https://image.flaticon.com/icons/svg/122/122324.svg" alt="stop button"/>
-              </button>
-              <button className="playback pauseBtn is-centered">
-                <img src="https://image.flaticon.com/icons/svg/122/122326.svg" alt="pause button"/>
-              </button>
-              <button className="playback playBtn is-centered">
-                <img src="https://image.flaticon.com/icons/svg/122/122323.svg" alt="play button"/>
-              </button>
-              <button className="toggle-pos" onClick={this.toggleToolBoxPosition.bind(this)}>
-                <img src="https://image.flaticon.com/icons/svg/122/122303.svg" alt="toggle toolbox view button"/>
-              </button>
-            </div>
-            <div className="playback-buttons">
-              <button className="playback to-beg-btn is-centered">
-                <img src="https://image.flaticon.com/icons/svg/122/122329.svg" alt="reverse to beginning"/>
-              </button>
-              <button className="playback rev-btn is-centered">
-                <img src="https://image.flaticon.com/icons/svg/122/122327.svg" alt="reverse button"/>
-              </button>
-              <button className="playback ff-btn is-centered">
-                <img src="https://image.flaticon.com/icons/svg/122/122328.svg" alt="fast-forward button"/>
-              </button>
-              <button className="playback to-end-btn is-centered">
-                <img src="https://image.flaticon.com/icons/svg/122/122330.svg" alt="fast-forward to end"/>
-              </button>
-            </div>
-          </div>
+          <Playback/> 
           <section className="notation-selectors is-flex">
             <div className="note-lengths-container selectors is-centered">
               <h3>Measure</h3>
