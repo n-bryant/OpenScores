aad3c51ccc3db98b83b3c7f20c904e2e606f1c31
Scott Hsu <scotthsumail@yahoo.com>
2017-03-20 08:21:49 -0400

    solidified a theme for the profile page

---
+++ open-scores-app/src/components/profile/Logo.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="main-logo-container">
+                <section className="logo-container">
+                    <img className="logo-image" src="https://image.flaticon.com/icons/svg/118/118328.svg"></img>
+                    <p className="logo-text">Open $cour$e</p>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Logo;
