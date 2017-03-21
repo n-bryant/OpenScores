0c8b6d944ff8855a8632368b0b28d389a6631084
Scott Hsu <scotthsumail@yahoo.com>
2017-03-21 16:50:42 -0400

    added chat form and minor styles

---
+++ open-scores-app/src/components/loginForm/MainForm.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import FormHeader from './FormHeader';
+
+class MainForm extends Component {
+    render() {
+        return (
+            <div className="main-form-container">
+                <section className="main-form"></section>
+            </div>
+        );
+    }
+}
+
+export default MainForm;
