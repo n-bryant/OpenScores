bfa0aadffbdef72107dfa32ce99fc7b85d96fd61
Scott Hsu <scotthsumail@yahoo.com>
2017-03-22 17:49:18 -0400

    fixed socket io

--- open-scores-app/src/server.js
+++ open-scores-app/src/server.js
@@ -3,12 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + 'public');
+    res.sendFile(__dirname + './index.html');
 });
 
 io.on('connection', function(socket) {
     console.log('a user connected');
-  socket.on('disconnect', function(socket){
+  socket.on('disconnect', function(){
     console.log('user disconnected');
     socket.on('chat message', function(msg){
       console.log('message: ' + msg);
