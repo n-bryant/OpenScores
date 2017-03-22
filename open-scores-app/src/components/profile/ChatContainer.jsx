2b63647de9339a984fc253dabe1eb28bd9d3d8bd
Scott Hsu <scotthsumail@yahoo.com>
2017-03-22 16:55:48 -0400

    minor style changes and socket io work

--- open-scores-app/src/components/profile/ChatContainer.jsx
+++ open-scores-app/src/components/profile/ChatContainer.jsx
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
+import io from 'socket.io-client';
+import $ from 'jquery';
+
+
+// (function() {
+// let socket = io();
+// document.querySelector('form').addEventListener('submit', () => {
+//   socket.emit('chat message', document.getElementById('m').value());
+//   document.getElementById('m').value('');
+//   return false;
+// });
+// });
 
 class ChatContainer extends Component {
+
+componentDidMount() {
+  let socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+}
+
     constructor() {
         super();
         this.chatApp = this.chatApp.bind(this);
+console.log(io(`http://localhost:3001`));
     }
+
     chatApp(event) {
-        console.log("in");
         event.preventDefault();
         console.log(this.chatInput.value);
     }
@@ -15,7 +38,7 @@ class ChatContainer extends Component {
         return (
             <div>
                 <ul id="messages"></ul>
-                <form action="" onSubmit={this.chatApp.bind(this)}>
+                <form className="chat-form-container" action=""onSubmit={this.chatApp.bind(this)}>
                     <input id="m" autocomplete="off" required placeholder="BE SOCIAL" ref={(input) => {
                         this.chatInput = input
                     }}/>
--- open-scores-app/src/components/profile/Profile.jsx
+++ open-scores-app/src/components/profile/Profile.jsx
@@ -11,7 +11,6 @@ class Profile extends Component {
                 <MainHeader/>
                 <UserCompositions/>
                 <MainSideBar/>
-                <ChatContainer/>
             </section>
         );
     }
